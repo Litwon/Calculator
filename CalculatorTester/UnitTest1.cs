52ac5367b288be36376121dfdcba9f4f847e8ffe
Litwon <2054052050quoc@ou.edu.vn>
2022-10-29 14:42:31 +0700

    new

---
+++ CalculatorTester/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApplication1;
+
+namespace CalculatorTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Calculation cal;
+        [TestInitialize]
+        public void SetUp()
+        {
+            cal = new Calculation(10, 5);
+        }
+        [TestMethod]
+        public void TestAddOperator()
+        {
+            Assert.AreEqual(cal.Execute("+"), 15);
+        }
+        [TestMethod]
+        public void TestSubOperator()
+        {
+            Assert.AreEqual(cal.Execute("-"), 5);
+        }
+        [TestMethod]
+        public void TestMulOperator()
+        {
+            Assert.AreEqual(cal.Execute("*"), 50);
+        }
+        [TestMethod]
+        public void TestDivOperator()
+        {
+            Assert.AreEqual(cal.Execute("/"), 2);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestDivByZero()
+        {
+            Calculation c = new Calculation(2, 0);
+            c.Execute("/");
+        }
+        public TestContext TestContext { get; set; }
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+         @".\TextData.csv", "TextData#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestWithDataSource()
+        {
+            int a = int.Parse(TestContext.DataRow[0].ToString());
+            int b = int.Parse(TestContext.DataRow[1].ToString());
+            string operation = TestContext.DataRow[2].ToString();
+            operation = operation.Remove(0, 1);
+            int expected = int.Parse(TestContext.DataRow[3].ToString());
+            Calculation c = new Calculation(a, b);
+            int actual = c.Execute(operation);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
